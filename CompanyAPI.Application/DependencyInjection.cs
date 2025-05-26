f24f220fb25ba71328bdf68c00e8a6ca8991e03f
CORASYSTEMS\pshannon <pshannon@corasystems.com>
2025-05-26 20:58:00 +0100

    removed automapper for now as not decided whether to use it or not yet, keeping it simple for now

--- CompanyAPI.Application/DependencyInjection.cs
+++ CompanyAPI.Application/DependencyInjection.cs
@@ -8,10 +8,6 @@ namespace CompanyAPI.Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
-
-            services.AddAutoMapper(Assembly.GetExecutingAssembly());
-
-
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
             return services;
         }
